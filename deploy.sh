4c42650c5a40066a88019de2aec05859fecb5b2c
ken5scal <kengoscal@gmail.com>
2016-11-24 01:53:47 +0900

    Hoge

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-set -ex &&
-    sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}
+set -e
+
+sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:latest
+if [[ "$CIRCLE_TAG" =~ v[0-9]+(\.[0-9]+)* ]]; then
+    docker tag asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:$CIRCLE_SHA1 asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:${CIRCLE_TAG}
+    sudo gcloud docker push asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:$CIRCLE_TAG
+fi
