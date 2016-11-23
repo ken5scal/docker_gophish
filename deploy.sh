0dd2e3755307eb9e981f6123d7c53996acbdceee
ken5scal <kengoscal@gmail.com>
2016-11-24 02:16:07 +0900

    Fix command

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:latest
 if [[ "$CIRCLE_TAG" =~ v[0-9]+(\.[0-9]+)* ]]; then
     docker tag asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:$CIRCLE_SHA1 asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:${CIRCLE_TAG}
-    sudo gcloud docker push asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:$CIRCLE_TAG
+    sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/${PROJECT_NAME}/${CONTAINER_NAME}:$CIRCLE_TAG
 fi
