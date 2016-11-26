3b873d07a3aa3a8c180572ebde1dbe601cd9f575
ken5scal <kengoscal@gmail.com>
2016-11-27 00:09:12 +0900

    Add rest api/gcloud command line for opening firewall configuration

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,15 @@ fi
                                                               
 sudo kubectl create -f gophish-deployment.yaml
 sudo kubectl create -f gophish-node-service.yaml
+
+#NodePort requires opening firewall configuration.
+#gcloud compute --project "gophish-150317" firewall-rules create --network "default"
+#POST https://www.googleapis.com/compute/v1/projects/gophish-150317/global/firewalls
+#{
+#    "kind": "compute#firewall",
+#    "sourceRanges": [],
+#    "sourceTags": [],
+#    "targetTags": [],
+#    "allowed": [],
+#    "network": "projects/gophish-150317/global/networks/default"
+#}
